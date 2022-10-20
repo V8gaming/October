02d0140079487681b2dfb0696432dc9c870b9b1a
Sam <samvoss69@gmail.com>
2022-10-20 16:08:16 +1100

    redo1

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,63 @@
-use iced::{executor, Application, Command, Settings, Text, Length, Color, Button, button, Renderer, Element};
-use iced::widget::{Container, Column, Row};
-use iced_wgpu::Backend;
+use iced::{button, Button, Element, Sandbox, Settings, Text, Container, Length, Column, Row};
 
-pub fn main() -> iced::Result {
-    Html::run(Settings::default())
-}
-fn rgbconvert(rgb: [i32; 3]) -> Color {
-    let r = rgb[0] as f32 / 255.0;
-    let g = rgb[1] as f32/ 255.0;
-    let b = rgb[2] as f32 / 255.0;
-    let a: f32 = 1.0;
-    //println!("{},{},{},{}", r,g,b,a);
-    return Color {r,g,b,a};
+static CHARLIST: [&str; 4] = ["a","b","c", "d"];
+#[derive(Default)]
+struct MyButton {
+    button_state1: button::State, 
+    button_state2: button::State,
+    button_state3: button::State,
 }
 
-struct Html {
-    buttonclicked: Message
+#[derive(Debug, Clone)]
+enum Message {
+    ButtonPressed1,
+    ButtonPressed2,
+    ButtonPressed3,
 }
 
-#[derive(Clone,Debug)]
-pub enum Message {
-    ButtonClicked,
-}
-
-impl Application for Html {
-    type Executor = executor::Default;
+impl Sandbox for MyButton {
     type Message = Message;
-    type Flags = ();
 
-    fn new(_flags: ()) -> (Html, Command<Message>) {
-        (Html { buttonclicked: Message::ButtonClicked }, Command::none())
+    fn new() -> Self {
+        Self::default()
     }
 
     fn title(&self) -> String {
-        String::from("Bresenham's line algorithm")
+        String::from("Button")
     }
 
-    fn update(&mut self, _message: Message) -> Command<Message> {
-        Command::none()
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ButtonPressed1 => println!("Button pressed"),
+            Message::ButtonPressed2 => println!("Button pressed"),
+            Message::ButtonPressed3 => println!("Button pressed"),
+        }
     }
-    //90.0,159.0,184.0
-    //0.35, 0.62, 0.72
+
     fn view(&mut self) -> Element<Message> {
-        let text1 = Text::new("Bresenham's line algorithm")
-        .size(30)
-        .color(rgbconvert([90,159,184]))
-        .horizontal_alignment(iced::alignment::Horizontal::Center)
-        .width(Length::Fill);
-
-        let text2 = Text::new("About the code")
-        .size(20)
-        .color(rgbconvert([90,159,184]));
-
-        let text3 = Text::new(
-        "The code in this article was written using Code::Blocks and SDL 2.
-        You can read here a guide to install this sofware.
-        Although it is based on SDL, I don't use its functions directly. I have written a small library with a few basic
-        functions to ease the understanding and the portability to another language.
-        You can read more about this lib here")
-        .size(15);
+        let text1 = Text::new(format!("text")).height(Length::Units(250)).size(100);
         
-        /*
-        let mut state1 = button::State::new();
-        let mut state2 = button::State::new();
-        let mut state3 = button::State::new();
-        let menu1 = Button::new(&mut state1, Text::new("Home")).on_press(Message::ButtonClicked);
-        let menu2 = Button::new(&mut state2, Text::new("Curves")).on_press(Message::ButtonClicked);
-        let menu3 = Button::new(&mut state3, Text::new("Contact")).on_press(Message::ButtonClicked);
-        */
-        let menu1 = Text::new("A").horizontal_alignment(iced::alignment::Horizontal::Left).width(Length::Fill);
-        let menu2 = Text::new("B").horizontal_alignment(iced::alignment::Horizontal::Center).width(Length::Fill);
-        let menu3 = Text::new("C").horizontal_alignment(iced::alignment::Horizontal::Right).width(Length::Fill);
-        let banner = Row::new().push(menu1).push(menu2).push(menu3).width(Length::Fill);
-        //let content = Column::new().push(text1).push(text2).push(text3);
-        
-        let content = Column::new().push(banner).push(text1).push(text2).push(text3);
+        let button1 = Button::new(&mut self.button_state1, Text::new("A"))
+            .on_press(Message::ButtonPressed1);
 
-        Container::new(content)
+        let button2 = Button::new(&mut self.button_state2, Text::new("B"))
+            .on_press(Message::ButtonPressed2);
+
+        let button3 = Button::new(&mut self.button_state3, Text::new("C"))
+            .on_press(Message::ButtonPressed3);
+
+        let row1 = Row::new().push(button1).push(button2).push(button3);
+        let column1 = Column::new().push(text1).push(row1);
+        Container::new(column1)
             .padding(100)
             .width(Length::Fill)
             .height(Length::Fill)
-            //.center_x()
-            //.center_y()
+            .center_x()
+            .center_y()
             .into()
     }
+}
+
+fn main() -> iced::Result {
+    MyButton::run(Settings::default())
 }
