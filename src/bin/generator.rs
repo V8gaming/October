de65ee2ef43f61bddbbb815d9e9f863f413cb725
Sam Voss <samvoss69@gmail.com>
2022-10-24 15:55:40 +1100

    many progress

--- src/bin/generator.rs
+++ src/bin/generator.rs
@@ -1,4 +1,13 @@
 use std::io::Write;
+
+fn some_kind_of_uppercase_first_letter(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
+
 fn main() {
     let charlist: [&str; 26] = ["a","b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let charlist2: [&str; 20] = [
@@ -9,35 +18,102 @@ fn main() {
     "í",
     "ế","ẹ"
     ];
-
+    let customstates: [&str; 3] = ["submit","space","delete"];
+    let customfunctions: [&str; 3] = ["sumbitfn()", "pushfn(\" \")",  "popfn()"];
+    let mut custombuttons = Vec::new();
+    for i in customstates {
+        custombuttons.push(some_kind_of_uppercase_first_letter(i).to_owned())
+    }
+    
     let mut file = std::fs::File::create("./src/bin/generated.rs").expect("create failed");
     file.write_all(format!("use iced::{{button, Button, Element, Sandbox, Settings, Text, Container, Length, Column, Row, window}};\n").as_bytes()).expect("write failed");
     file.write_all(format!("use iced::window::Position::Specific;\n").as_bytes()).expect("write failed");
     file.write_all(format!("use iced::window::Icon;\n").as_bytes()).expect("write failed");
+    file.write_all(format!("use global::Global;\n").as_bytes()).expect("write failed");
     
+    // static LETTERS: Global<Vec<String>> = Global::new();
+    file.write_all(format!("static LETTERS: Global<Vec<String>> = Global::new();\n").as_bytes()).expect("write failed");
+
     file.write_all("\n".as_bytes()).expect("write failed");
     file.write_all(format!("#[derive(Default, Clone)]\nstruct MyButton {{\n").as_bytes()).expect("write failed");
+
+    
+    for i in customstates {
+        file.write_all(format!("    {}_state: button::State,\n",i).as_bytes()).expect("write failed");
+    }
+
     for i in 0..charlist.len()+charlist2.len() {
         file.write_all(format!("    button_state{}: button::State,\n", i).as_bytes()).expect("write failed");
     }
     file.write_all(format!("}}\n").as_bytes()).expect("write failed");
     file.write_all("\n".as_bytes()).expect("write failed");
     file.write_all("#[derive(Debug, Clone)]".as_bytes()).expect("write failed");
+
+    // emum Message
     file.write_all("enum Message {\n".as_bytes()).expect("write failed");
+    for i in &custombuttons {
+        file.write_all(format!("    {}Button,\n",i).as_bytes()).expect("write failed");
+    }
     for i in 0..charlist.len()+charlist2.len() {
         file.write_all(format!("    ButtonPressed{},\n", i).as_bytes()).expect("write failed");
     }
     file.write_all("}\n".as_bytes()).expect("write failed");
+
+    /* 
+    fn pushfn(letter: &str) {
+        LETTERS.lock_mut().unwrap().push(letter.to_string());
+        println!("ADDED {} TO {}", letter,LETTERS.lock_mut().unwrap().concat());
+    }
+    */
+    file.write_all("fn pushfn(letter: &str) {\n".as_bytes()).expect("write failed");
+    file.write_all("    LETTERS.lock_mut().unwrap().push(letter.to_string());\n".as_bytes()).expect("write failed");
+    file.write_all("    println!(\"ADDED {} TO {}\", letter,LETTERS.lock_mut().unwrap().concat());\n".as_bytes()).expect("write failed");
+    file.write_all("}\n".as_bytes()).expect("write failed");
+
+    /* 
+    fn sumbitfn() {
+        println!("{}",LETTERS.lock_mut().unwrap().concat());
+    }
+    */
+    file.write_all("fn sumbitfn() {\n".as_bytes()).expect("write failed");
+    file.write_all("    println!(\"{}\",LETTERS.lock_mut().unwrap().concat());\n".as_bytes()).expect("write failed");
+    file.write_all("}\n".as_bytes()).expect("write failed");
+
+    /* 
+    fn popfn() {
+        if LETTERS.lock_mut().unwrap().len() != 0 {
+            LETTERS.lock_mut().unwrap().pop();
+            println!("{}",LETTERS.lock_mut().unwrap().concat());
+        } 
+    }
+    */
+    file.write_all("fn popfn() {\n".as_bytes()).expect("write failed");
+    file.write_all("    if LETTERS.lock_mut().unwrap().len() != 0 {\n".as_bytes()).expect("write failed");
+    file.write_all("        LETTERS.lock_mut().unwrap().pop();\n".as_bytes()).expect("write failed");
+    file.write_all("        println!(\"{}\",LETTERS.lock_mut().unwrap().concat());\n".as_bytes()).expect("write failed");
+    file.write_all("    }\n".as_bytes()).expect("write failed");
+    file.write_all("}\n".as_bytes()).expect("write failed");
+
+    // impl Sandbox for MyButton
     file.write_all("impl Sandbox for MyButton {\n".as_bytes()).expect("write failed");
     file.write_all("    type Message = Message;\n   fn new() -> Self {\n        Self::default()\n  }".as_bytes()).expect("write failed");
 
+    // fn title
     file.write_all("\n  fn title(&self) -> String {\n        String::from(\"Button\")\n   }".as_bytes()).expect("write failed");
+
+
+    // fn update
     file.write_all("\n  fn update(&mut self, message: Message) {\n        match message {\n".as_bytes()).expect("write failed");
+
+    for i in 0..customstates.len() {
+        file.write_all(format!("      Message::{}Button => {},\n", custombuttons[i], customfunctions[i]).as_bytes()).expect("write failed");
+    }
+
     for i in 0..charlist.len()+charlist2.len() {
         if i < charlist.len(){
-            file.write_all(format!("        Message::ButtonPressed{} => println!(\"{}\"),\n", i, charlist[i]).as_bytes()).expect("write failed");
+            file.write_all(format!("        Message::ButtonPressed{} => pushfn(\"{}\"),\n", i, charlist[i]).as_bytes()).expect("write failed");
         } else if i >= charlist.len() {
-            file.write_all(format!("        Message::ButtonPressed{} => println!(\"{}\"),\n", i, charlist2[i-charlist.len()]).as_bytes()).expect("write failed");
+            file.write_all(format!("        Message::ButtonPressed{} => pushfn(\"{}\"),\n", i, charlist2[i-charlist.len()]).as_bytes()).expect("write failed");
         }
         
     }
@@ -45,35 +121,56 @@ fn main() {
 
     file.write_all("    }\n".as_bytes()).expect("write failed");
 
+    // fn view
     file.write_all("\n  fn view(&mut self) -> Element<Message> {".as_bytes()).expect("write failed");
     file.write_all("\n     fn add_button<'a>(a: &'a mut button::State,b: &'a str,c: Message) -> Button<'a, Message> {\n".as_bytes()).expect("write failed");
     file.write_all("\n          return Button::new(a, Text::new(format!(\"{}\",b))).on_press(c);\n".as_bytes()).expect("write failed");
     file.write_all("\n      }\n".as_bytes()).expect("write failed");
 
-    file.write_all("    let text1 = Text::new(format!(\"text\")).height(Length::Units(250)).size(100);\n".as_bytes()).expect("write failed");
+    // text1
+    // let text1 = Text::new(format!("{}", LETTERS.lock_mut().unwrap().concat())).height(Length::Units(250)).size(100);
+    file.write_all("    let text1 = Text::new(format!(\"{}\", LETTERS.lock_mut().unwrap().concat())).height(Length::Units(250)).size(100);\n".as_bytes()).expect("write failed");
+
+    // button1
     file.write_all("    let buttons1 = [\n".as_bytes()).expect("write failed");
     for i in 0..charlist.len() {
         file.write_all(format!("        add_button(&mut self.button_state{}, \"{}\", Message::ButtonPressed{}),\n",i,charlist[i], i).as_bytes()).expect("write failed");
     }
     file.write_all("    ];\n".as_bytes()).expect("write failed");
 
+    // button2
     file.write_all("    let buttons2 = [\n".as_bytes()).expect("write failed");
     for i in 0..charlist2.len() {
         file.write_all(format!("        add_button(&mut self.button_state{}, \"{}\", Message::ButtonPressed{}),\n",i+charlist.len(),charlist2[i], i+charlist.len()).as_bytes()).expect("write failed");
     }
     file.write_all("    ];\n".as_bytes()).expect("write failed");
 
+    // submit
+    // let submit = add_button(&mut self.submit_state, "submit", Message::SubmitButton);
+    for i in 0..customstates.len() {
+        file.write_all(format!("    let {} = add_button(&mut self.{}_state, \"{}\", Message::{}Button);\n", customstates[i], customstates[i],customstates[i], custombuttons[i]).as_bytes()).expect("write failed");
+    }
+
+    // userrow
+    file.write_all("    let mut userrow = Row::new();\n".as_bytes()).expect("write failed");
+    for i in customstates {
+        file.write_all(format!("    userrow = userrow.push({});\n", i).as_bytes()).expect("write failed");
+    }
+   
+
+    // row1
     file.write_all("    let mut row1 = Row::new();\n".as_bytes()).expect("write failed");
     file.write_all("    for button in buttons1 {\n".as_bytes()).expect("write failed");
     file.write_all("        row1 = row1.push(button);\n".as_bytes()).expect("write failed");
     file.write_all("    };\n".as_bytes()).expect("write failed");
 
+    // row2
     file.write_all("    let mut row2 = Row::new();\n".as_bytes()).expect("write failed");
     file.write_all("    for button in buttons2 {\n".as_bytes()).expect("write failed");
     file.write_all("        row2 = row2.push(button);\n".as_bytes()).expect("write failed");
     file.write_all("    };\n".as_bytes()).expect("write failed");
 
-    file.write_all("    let column1 = Column::new().push(text1).push(row1).push(row2).width(Length::Fill).align_items(iced::Alignment::Center);\n".as_bytes()).expect("write failed");
+    file.write_all("    let column1 = Column::new().push(text1).push(userrow).push(row1).push(row2).width(Length::Fill).align_items(iced::Alignment::Center);\n".as_bytes()).expect("write failed");
     file.write_all("    Container::new(column1)\n".as_bytes()).expect("write failed");
     file.write_all("    .padding(100)\n    .width(Length::Fill)\n    .height(Length::Fill)\n    .center_x()\n    .center_y()\n    .into()".as_bytes()).expect("write failed");
 
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,14 @@
 use iced::{button, Button, Element, Sandbox, Settings, Text, Container, Length, Column, Row, window};
 use iced::window::Position::Specific;
 use iced::window::Icon;
+use global::Global;
 
+static LETTERS: Global<Vec<String>> = Global::new();
 #[derive(Default, Clone)]
 struct MyButton {
+    submit_state: button::State,
+    space_state: button::State,
+    delete_state: button::State,
     button_state0: button::State, 
     button_state1: button::State,
     button_state2: button::State,
@@ -12,12 +17,32 @@ struct MyButton {
 
 #[derive(Debug, Clone)]
 enum Message {
+    SubmitButton,
+    SpaceButton,
+    DeleteButton,
     ButtonPressed0,
     ButtonPressed1,
     ButtonPressed2,
     ButtonPressed3,
 }
 
+
+fn pushfn(letter: &str) {
+    LETTERS.lock_mut().unwrap().push(letter.to_string());
+    println!("ADDED {} TO {}", letter,LETTERS.lock_mut().unwrap().concat());
+}
+
+fn sumbitfn() {
+    println!("{}",LETTERS.lock_mut().unwrap().concat());
+}
+
+fn popfn() {
+    if LETTERS.lock_mut().unwrap().len() != 0 {
+        LETTERS.lock_mut().unwrap().pop();
+        println!("{}",LETTERS.lock_mut().unwrap().concat());
+    } 
+    
+}
 
 impl Sandbox for MyButton {
     type Message = Message;
@@ -30,15 +55,19 @@ impl Sandbox for MyButton {
     fn title(&self) -> String {
         String::from("Button")
     }
-
+    
     fn update(&mut self, message: Message) {
-        let mut letters: Vec<&str> = Vec::new();
         match message {
-            Message::ButtonPressed0 => letters.push("a"),
-            Message::ButtonPressed1 => letters.push("b"),
-            Message::ButtonPressed2 => letters.push("c"),
-            Message::ButtonPressed3 => letters.push("d"),
-        }
+            Message::SubmitButton => sumbitfn(),
+            Message::SpaceButton => pushfn(" "),
+            Message::DeleteButton => popfn(),
+            Message::ButtonPressed0 => pushfn("a"),
+            Message::ButtonPressed1 => pushfn("b"),
+            Message::ButtonPressed2 => pushfn("c"),
+            Message::ButtonPressed3 => pushfn("d"),
+
+        };
+
     }
 
     fn view(&mut self) -> Element<Message> {
@@ -47,7 +76,7 @@ impl Sandbox for MyButton {
             return Button::new(a, Text::new(format!("{}",b))).on_press(c);
         }
         
-        let text1 = Text::new(format!("text")).height(Length::Units(250)).size(100);
+        let text1 = Text::new(format!("{}", LETTERS.lock_mut().unwrap().concat())).height(Length::Units(250)).size(100);
 
         /* 
         let mut buttons: Vec<Button<Message>> = Vec::new();
@@ -72,12 +101,20 @@ impl Sandbox for MyButton {
         let button3 = Button::new(&mut self.button_state3, Text::new("C"))
             .on_press(Message::ButtonPressed3);
         */
+        let submit = add_button(&mut self.submit_state, "submit", Message::SubmitButton);
+        let space = add_button(&mut self.space_state, "space", Message::SpaceButton);
+        let delete = add_button(&mut self.delete_state, "delete", Message::DeleteButton);
+        
+
+        let mut userrow = Row::new();
+        userrow = userrow.push(submit).push(space).push(delete);
+
 
         let mut row1 = Row::new();
         for button in buttons {
             row1 = row1.push(button);
         };
-        let column1 = Column::new().push(text1).push(row1).width(Length::Fill).align_items(iced::Alignment::Center);
+        let column1 = Column::new().push(text1).push(userrow).push(row1).width(Length::Fill).align_items(iced::Alignment::Center);
         Container::new(column1)
             .padding(100)
             .width(Length::Fill)
